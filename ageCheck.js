5dd03ad0b869e42a8de6232cf3ab49af07abc359
Marjeremy <gaoat856@gmail.com>
2024-10-10 12:19:18 +0800

    ageCheck.js commit

---
+++ ageCheck.js
@@ -0,0 +1,16 @@
+function ageCheck(){
+    let age = prompt("Enter your age");
+    age = parseInt(age);
+
+    if (age < 13){
+        alert("You are a child.");
+    }else if (age >= 13 && age < 20){
+        alert("You are a teenager.");
+    }else{
+        alert("you are an adult.");
+    }
+    
+
+
+}
+    
